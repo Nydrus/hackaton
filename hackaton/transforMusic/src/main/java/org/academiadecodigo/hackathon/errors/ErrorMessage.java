5e4e3638614862408718d28006967e945b082026
Miguel Raposo <miguel.p.raposo89@gmail.com>
2022-08-06 16:50:21 +0100

    added project

--- hackaton/transforMusic/src/main/java/org/academiadecodigo/hackathon/errors/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package org.academiadecodigo.hackathon.errors;
-
-/**
- * Error messages to be used throughout the application
- */
-public class ErrorMessage {
-    public static final String CUSTOMER_NOT_FOUND = "Customer does not exist";
-}
